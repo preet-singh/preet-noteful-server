8b6866326617dcbba97171d2b4cea19cca5d8faf
preet-singh <preetsingh13@gmail.com>
2019-10-08 15:33:41 -0500

    migrations and seeds complete

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES 
+    ('Kitchen'),
+    ('Bathroom'),
+    ('Master Bedroom');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_notes (note_name, note_content, folder_id)
+VALUES 
+    ('Kitchen inventory', 'first list goes here', 1),
+    ('Bathroom inventory', 'second list goes here', 2),
+    ('Master bedroom inventory', 'third list goes here', 3);
